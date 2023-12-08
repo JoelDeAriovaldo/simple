3257238428e7879daab10d5654e4928dc29b14ee
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2023-12-08 12:07:12 +0200

    Salvar dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { StorageService } from '../services/storage.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,10 +11,22 @@ export class HomePage {
   endereco: any;
   email: any;
   celular: any;
+  
+  constructor(public storage: StorageService) {}
 
-  constructor() {}
+  salvarDados() {
+    let info = {
+      nome: this.nome,
+      idade: this.idade,
+      endereco: this.endereco,
+      email: this.email,
+      celular: this.celular,
+    };
 
-  salvarDados(){
-    alert('Nome: '+this.nome)
+    this.storage.formData = info;
+
+    alert('Salvo com sucesso');
+
+    console.log(this.storage.formData);
   }
 }
